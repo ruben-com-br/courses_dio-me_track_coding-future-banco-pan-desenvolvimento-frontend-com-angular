2a1933d0d87f245cedfc4d490b9ea216bd95e92f
Ruben Borges Ramos <ruben.ramos.ps@ojc.com.br>
2024-04-14 18:10:24 -0300

    02-09 Convertendo o modelo do PokeApi para nosso modelo

--- 02 - Fundamentos de Front-End/00 - Projeto/assets/js/main.js
+++ 02 - Fundamentos de Front-End/00 - Projeto/assets/js/main.js
@@ -1,20 +1,16 @@
 
 // http-server .
 
-function convertPokemonTypesToLi(pokemonTypes){
-  return pokemonTypes.map((type) => `<li class="type">${type}</li>`)
-}
-
 function convertPokemonToLi(pokemon) {
   return `
   <li class="pokemon">
-        <span class="number">#${pokemon.order}</span>
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
           <ol class="types">
-            ${convertPokemonTypesToLi(pokemon.types).join('')}
+            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
           </ol>
-          <img src="${pokemon.sprites.other.dream_world.front_default}"
+          <img src="${pokemon.photo}"
                alt="${pokemon.name}"
           >
         </div>
--- 02 - Fundamentos de Front-End/00 - Projeto/assets/js/poke-api.js
+++ 02 - Fundamentos de Front-End/00 - Projeto/assets/js/poke-api.js
@@ -1,5 +1,20 @@
 const pokeApi = {};
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.order;
+  pokemon.name = pokeDetail.name;
+  
+  const types = pokeDetail.types.map((typeSlot)=>typeSlot.type.name);
+  const [type] = types
+
+  pokemon.types = types 
+  pokemon.type = type
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+  return pokemon;
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 20) => {
   
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
@@ -9,7 +24,7 @@ pokeApi.getPokemons = (offset = 0, limit = 20) => {
   .then( (jsonBody) => jsonBody.results)
   .then( (pokemons) => pokemons.map(pokeApi.getPokemonsDetails))
   .then( (detailRequests) => Promise.all(detailRequests))
-  .then( (pokemonDetails) => pokemonDetails )
+  .then( (pokemonDetails) => pokemonDetails.map(convertPokeApiDetailToPokemon) )
 
 } 
 
