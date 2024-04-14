00e9917298daf1f3ba9d9dbdba748cfba2b5c74d
Ruben Borges Ramos <ruben.ramos.ps@ojc.com.br>
2024-04-14 17:20:02 -0300

    02-09 separando o consumo API da manipulacao HTML

--- 02 - Fundamentos de Front-End/00 - Projeto/assets/js/main.js
+++ 02 - Fundamentos de Front-End/00 - Projeto/assets/js/main.js
@@ -1,11 +1,5 @@
 
 // http-server .
-
-const offset = 0
-const limit  = 10
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
   return `
   <li class="pokemon">
@@ -26,18 +20,16 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-  .then( (response) => response.json())
-  .then( (jsonBody) => jsonBody.results)
+pokeApi.getPokemons()
   .then( (pokemons) => {
-    //debugger
-    console.log(pokemons)
+    const listItems = []
 
     for (let i = 0; i < pokemons.length ; i++){
-      const pokemon = pokemons[i];
-      pokemonList.innerHTML += convertPokemonToLi(pokemon);
-
+      const pokemon = pokemons[i]
+      listItems.push(convertPokemonToLi(pokemon))
     }
+
+    pokemonList.innerHTML += listItems
   })
   .catch( (error) => console.log(error))
 
