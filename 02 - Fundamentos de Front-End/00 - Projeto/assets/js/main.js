d45df28187714626841511c7e82a2023e7fb211d
Ruben Borges Ramos <ruben.ramos.ps@ojc.com.br>
2024-04-14 16:09:37 -0300

    02-08 Adicionando os tipos de pokemon

--- 02 - Fundamentos de Front-End/00 - Projeto/assets/js/main.js
+++ 02 - Fundamentos de Front-End/00 - Projeto/assets/js/main.js
@@ -10,15 +10,15 @@
 
 
 
+// http-server .
 
-/*
 const offset = 0
 const limit  = 10
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-  console.log(response);
-})
+fetch(url)
+  .then( (response) => console.log(response))
+  .then( (response) => console.log(response))
+  .catch( (error) => console.log(error))
 
-*/
